fb86944cd6466e178ebf9e2b26e9fe082bd02869
Anupriya5699 <anuwriteslife@gmail.com>
2025-01-10 14:08:41 +0530

    Added three more methods

--- src/test/java/com/githubnew/GitHubTaskNew.java
+++ src/test/java/com/githubnew/GitHubTaskNew.java
@@ -13,4 +13,16 @@ private void sample3() {
 	System.out.println("Anupriya");
 
 }
+private void nmeth() {
+	System.out.println("Janani");
+
+}
+private void nmeth1() {
+	System.out.println("janu");
+
+}
+private void nmeth2() {
+	System.out.println("janana");
+
+}
 }
