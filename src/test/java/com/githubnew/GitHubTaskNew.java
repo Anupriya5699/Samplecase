63ea3fd78d2bb736f962040b2befad7b30473c0d
Anupriya5699 <anuwriteslife@gmail.com>
2025-01-10 13:46:34 +0530

    Added three methods

---
+++ src/test/java/com/githubnew/GitHubTaskNew.java
@@ -0,0 +1,16 @@
+package com.githubnew;
+
+public class GitHubTaskNew {
+private void sample1() {
+	System.out.println("Anu");
+}
+
+private void sample2() {
+	System.out.println("Anu.n");
+
+}
+private void sample3() {
+	System.out.println("Anupriya");
+
+}
+}
